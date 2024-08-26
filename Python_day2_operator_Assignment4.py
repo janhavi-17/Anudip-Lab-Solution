f12a03c16803ddcfe59c75da21d65c0c34d89cc0
janhavi-17 <102974972+janhavi-17@users.noreply.github.com>
2024-08-26 10:52:23 +0530

    Add files via upload

---
+++ Python_day2_operator_Assignment4.py
@@ -0,0 +1,12 @@
+#4.Python program to find the area of a triangle whose sides are given
+
+Base = 6
+Height = 5
+
+#Area of Triangle
+Area = 0.5 * Base * Height
+
+print("Area of Trinagle = "+ str(Area))
+
+
+#ANS: Area of triangle is 15.0
